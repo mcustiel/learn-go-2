d7b9f0c364963e3bce80337ebe01022e6ed1b613
Mariano Custiel <jmcustiel@gmail.com>
2016-05-16 15:49:24 +0200

    Made some improvements, graph and queue are external deps now

--- duplicate-folders-finder.go
+++ duplicate-folders-finder.go
@@ -2,47 +2,45 @@
 package main
 
 import (
-	"container/list"
+	// "container/list"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/mcustiel/graph"
 )
 
 func main() {
-	var root Node = ScanDirToTree("C:\\test")
+	var graphInstance *graph.Graph = ScanDirToTree("C:\\test")
+	var equalityIndex float32 = 0.8
+
 	excluded := make(map[string]bool, 0)
 	orderedFiles := make(map[int]string, 0)
 	equals := make(map[string][]string)
+
 	current := 0
-	bfs(root, func(value string) {
-		fmt.Println("Saving directory ", value, " with index ", current)
+	graphInstance.Bfs(func(node *graph.Node) {
+		value := node.Value().(string)
 		orderedFiles[current] = value
 		current++
 	})
 
 	for index := 0; index < len(orderedFiles); index++ {
 		dirName := orderedFiles[index]
-		fmt.Println("Looking for duplicates of ", dirName)
 		if !isExcluded(dirName, excluded) {
 			for _, otherDirName := range orderedFiles {
 				if dirName != otherDirName && !isExcluded(otherDirName, excluded) {
-					fmt.Println("-- Comparing with", otherDirName)
-					if compareDirContents(GetDirectoryContents(dirName), GetDirectoryContents(otherDirName)) {
+					if calculateEqualityIndex(GetDirectoryContents(dirName), GetDirectoryContents(otherDirName)) >= equalityIndex {
 						if _, has := equals[dirName]; !has {
-							fmt.Println("-- Creating the key and list")
 							equals[dirName] = make([]string, 0)
 						}
-						fmt.Println("-- Adding duplicated to the list")
 						equals[dirName] = addToList(equals[dirName], otherDirName)
-						fmt.Println("-- Excluding duplicated from future searches")
 						excluded[otherDirName] = true
 					}
 				}
 			}
-		} else {
-			fmt.Println("It was already excluded")
 		}
 	}
 
@@ -63,9 +61,9 @@ func isExcluded(dirName string, excluded map[string]bool) bool {
 	return false
 }
 
-func compareDirContents(dir1 []os.FileInfo, dir2 []os.FileInfo) bool {
-	if len(dir1) == 0 {
-		return false
+func calculateEqualityIndex(dir1 []os.FileInfo, dir2 []os.FileInfo) float32 {
+	if len(dir1) == 0 || len(dir2) == 0 {
+		return 0
 	}
 	matches := 0
 	for _, fileInfo1 := range dir1 {
@@ -75,12 +73,10 @@ func compareDirContents(dir1 []os.FileInfo, dir2 []os.FileInfo) bool {
 			}
 		}
 	}
-	fmt.Println(float32(matches) / float32(len(dir1)))
-	return matches == len(dir1)
+	return float32(matches) / float32(len(dir1))
 }
 
 func addToList(stringList []string, element string) []string {
-	fmt.Println("Extending slice")
 	n := len(stringList)
 	if n == cap(stringList) {
 		newList := make([]string, len(stringList), cap(stringList)+1)
@@ -92,45 +88,18 @@ func addToList(stringList []string, element string) []string {
 	return stringList
 }
 
-func bfs(start Node, callback func(string)) {
-	visited := make(map[string]bool, 0)
-	walkTree(start, func(value string) {
-		fmt.Println("Setting ", value, " to not visited")
-		visited[value] = false
-	})
-
-	queue := list.New()
-	queue.PushBack(start)
-	for queue.Len() > 0 {
-		current := queue.Front().Value.(Node)
-		queue.Remove(queue.Front())
-		for e := current.children.Front(); e != nil; e = e.Next() {
-			currentNode := e.Value.(Node)
-			if passed := visited[currentNode.value]; !passed {
-				visited[currentNode.value] = true
-				queue.PushBack(currentNode)
-				callback(currentNode.value)
+func ScanDirToTree(dirName string) *graph.Graph {
+	var function func(string) *graph.Node
+	function = func(curDir string) *graph.Node {
+		node := graph.NewNode(curDir)
+		for _, file := range GetDirectoryContents(curDir) {
+			if file.IsDir() {
+				node.AddAdyacent(function(curDir + string(os.PathSeparator) + file.Name()))
 			}
 		}
+		return node
 	}
-}
-
-func walkTree(node Node, callback func(string)) {
-	callback(node.value)
-	for e := node.children.Front(); e != nil; e = e.Next() {
-		walkTree(e.Value.(Node), callback)
-	}
-}
-
-func ScanDirToTree(dirName string) Node {
-	graph := NewNode(dirName)
-
-	for _, file := range GetDirectoryContents(dirName) {
-		if file.IsDir() {
-			graph.children.PushBack(ScanDirToTree(dirName + string(os.PathSeparator) + file.Name()))
-		}
-	}
-	return graph
+	return graph.New(function(dirName))
 }
 
 func GetDirectoryContents(dirName string) []os.FileInfo {
